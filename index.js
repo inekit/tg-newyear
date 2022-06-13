83084d72d6e1d651e8c4dd3823a6f0784fd3a220
inekit <nicklzx@uni-dubna.ru>
2022-06-13 16:39:24 +0300

    webhook

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ console.log('started');
         );
         console.log('webhook is started')
 
-        const r = await bot.telegram.setWebhook(
+        /*const r = await bot.telegram.setWebhook(
            `https://${process.env.SERVER_IP}:8443/${TOKEN}`,
            {
              certificate: { source: "./cert.pem" },
@@ -50,8 +50,8 @@ console.log('started');
              allowed_updates,
              drop_pending_updates: true,
            }
-         );'
-         console.log('webhook is set')'
+         );
+         console.log('webhook is set')'*/
     } else {
         await bot.launch({
             allowedUpdates: allowed_updates,
