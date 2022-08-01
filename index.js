10300678a9127e5573f313720b37a99d1dd9a253
nicklzx <nicklzx@uni-dubna.ru>
2022-08-01 18:36:46 +0300

    FIRST

--- index.js
+++ index.js
@@ -3,63 +3,68 @@ const { readFileSync } = require("fs");
 const stages = require("./stages");
 const shortcuts = require("telegraf-steps-engine/shortcuts/shortcuts");
 const middlewares = require("telegraf-steps-engine/middlewares/middlewares");
-require('dotenv').config()
+require("dotenv").config();
 //const LocalSession = require('telegraf-session-local')
 
 const allowed_updates = ["message", "callback_query", "chat_member"];
-const TOKEN =
-    process.env.BOT_TOKEN;
+const TOKEN = process.env.BOT_TOKEN;
 
 const bot = new Telegraf(TOKEN);
 
-console.log('started');
+console.log("started");
 
+(async () => {
+  Object.assign(bot.context, shortcuts, middlewares);
 
-(async() => {
+  const ctx = { ...bot.context, telegram: bot.telegram };
 
-
-    Object.assign(bot.context, shortcuts, middlewares);
-
-    const ctx = {...bot.context, telegram: bot.telegram };
-
-    bot.use(session(),
-        /*(new LocalSession({ 
+  bot.use(
+    session(),
+    /*(new LocalSession({ 
            database: 'PublicStorage/sessions.json',
            storage: LocalSession.storageFileAsync,
            
          })).middleware(),*/
-        stages);
+    stages
+  );
 
-    if ("process.env.NODE_ENV']" === "production") {
-        bot.catch(console.error);
+  if ("production" === "production") {
+    bot.catch(console.error);
 
-        await bot.startWebhook(`/bot`, 
-            `/${TOKEN}`, {
-                key: readFileSync("./key.pem"),
-                cert: readFileSync("./cert.pem"),
-            },
-            8443
-        );
-        console.log('webhook is started')
+    const secretPath = `/telegraf/${bot.secretPathComponent()}`;
 
-        /*const r = await bot.telegram.setWebhook(
-           `https://${process.env.SERVER_IP}:8443/${TOKEN}`,
-           {
-             certificate: { source: "./cert.pem" },
-             ip_address: process.env.SERVER_IP,
-             allowed_updates,
-             drop_pending_updates: true,
-           }
-         );
-         console.log('webhook is set')'*/
-    } else {
-        await bot.launch({
-            allowedUpdates: allowed_updates,
-            dropPendingUpdates: true,
-        });
-    }
+    console.log(secretPath);
+
+    const tlsOptions = {
+      key: fs.readFileSync("/etc/ssl/certs/rootCA.key"),
+      cert: fs.readFileSync("/etc/ssl/certs/rootCA.crt"),
+      ca: [
+        // This is necessary only if the client uses a self-signed certificate.
+        fs.readFileSync("/etc/ssl/certs/185.178.47.201.crt"),
+      ],
+    };
+
+    bot.telegram
+      .setWebhook(`${process.env.SERVER_URI}${secretPath}`, {
+        certificate: { source: fs.readFileSync("/etc/ssl/certs/rootCA.crt") },
+        allowed_updates,
+        drop_pending_updates: true,
+      })
+      .then((r) => {
+        console.log(r);
+      });
+
+    //    console.log(await ctx.telegram.getWebhookInfo());
+    await bot.startWebhook(secretPath, tlsOptions, 443);
+
+    console.log(await ctx.telegram.getWebhookInfo());
+  } else {
+    await bot.launch({
+      allowedUpdates: allowed_updates,
+      dropPendingUpdates: true,
+    });
+  }
 })();
-
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
